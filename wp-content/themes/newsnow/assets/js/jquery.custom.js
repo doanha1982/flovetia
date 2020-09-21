ac5c50723b587dde897962493f92220670645e90
Ha Doan <44855379+doanha1982@users.noreply.github.com>
2020-09-21 11:45:16 +0700

    up

--- wp-content/themes/newsnow/assets/js/jquery.custom.js
+++ wp-content/themes/newsnow/assets/js/jquery.custom.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
         interval: 5000,
         ride: 'carousel'
     });
+
     $(document).on('click', '[data-toggle="lightbox"]', function(event) {
         event.preventDefault();
         $(this).ekkoLightbox({
